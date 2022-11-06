f2ce3a340c69cdbafe1e9c678d7e15b18675f30c
Aidan Boland <aidanboland1@icloud.com>
2022-11-06 19:08:15 +0000

    embed

--- embed.js
+++ embed.js
@@ -2,6 +2,8 @@ const { config } = require('dotenv');
 config();
 
 const { GatewayIntentBits } = require('discord.js');
+const { MessageEmbed } =  require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client({
     intents: [
@@ -11,3 +13,37 @@ const client = new Discord.Client({
         GatewayIntentBits.MessageContent,
     ],
 });
+
+const click = 'Click here to view message';
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.channels.cache.get(`${process.env.CHANNEL_ID}`).send('my name is Skylar White yo');
+});
+
+client.on('messageCreate', async (message) => {
+    console.log(`${message.author.id}: ${message.content}`)
+    const embed = {
+	    color: 0x0099ff,
+    	title: `${message.content}`,
+    	author: {
+    		name: `${message.author.tag}`,
+    		icon_url: `${message.author.avatarURL()}`
+    	},
+        fields: [
+            {name: `Current CO count: ${counterJSON.count}`, value: 'Crank up that atmospheric PPM'},
+        ],
+        description: `[${click}](${message.url})`,
+    	timestamp: new Date().toISOString(),
+    };
+
+    if (!message?.author.bot) {
+        message.channel.send(`this mf (${message.author}) said carbon monoxide ong :skull:`);
+        message.channel.send({embeds: [embed]});
+    }
+})
+
+
+
+client.login(`${process.env.DISCORD_TOKEN}`);
--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const client = new Discord.Client({
     ],
 });
 
+const click = 'Click here to view message';
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.channels.cache.get(`${process.env.CHANNEL_ID}`).send('yo');
@@ -26,8 +28,23 @@ client.on('messageCreate', async (input) => {
     let counterJSON = JSON.parse(fs.readFileSync('counter.json'));
     let message = input.content.toLowerCase();
     console.log(`${input.author} says: ${input.content}`);
+    const embed = {
+	    color: 0x0099ff,
+    	title: `${input.content}`,
+    	author: {
+    		name: `${input.author.tag}`,
+    		icon_url: `${input.author.avatarURL()}`
+    	},
+        fields: [
+            {name: '\u200b', value: '\u200b'},
+            {name: `Current CO count: ${counterJSON.count}`, value: 'Crank up that atmospheric PPM'},
+        ],
+        description: `[${click}](${input.url})`,
+    	timestamp: new Date().toISOString(),
+    };
     if ((message.includes('carbon'))&&(message.includes('monoxide'))&&(!input?.author.bot)) {
-        client.channels.cache.get(`${process.env.CHANNEL_ID}`).send(`:bangbang::bangbang::bangbang::bangbang::bangbang: \nthis mf (${input.author})said carbon monoxide ong :skull: \n \n"${input.content}" \n \n${input.url} \n \nCurrent CO count: ${counterJSON.count}, crank up that atmospheric PPM \n:bangbang::bangbang::bangbang::bangbang::bangbang:`);
+        client.channels.cache.get(`${process.env.CHANNEL_ID}`).send(`this mf (${input.author}) said carbon monoxide ong :skull:`);
+        client.channels.cache.get(`${process.env.CHANNEL_ID}`).send({embeds: [embed]});
         counterJSON.count++;
         console.log(counterJSON.count);
         //const embed = new Discord.Embed()
